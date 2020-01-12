4f09c454c684df7fe7674b5552587ac1deeb44c4
GustavoTch <GustavoTitoChoque1358923@gmail.com>
2020-01-12 19:15:01 -0400

    Change "Accra" for "La Paz"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:la_paz_app/main.dart';
+import 'package:trufi_core/trufi_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TrufiApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
